bca9562dc04809ce5ba3c9f7456e3c78d998d88b
Joey Fang <josudoey@gmail.com>
2019-07-05 18:07:48 +0800

    [why] refactor for codeclimate issue

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
+const add = function (tail, func, self, args) {
+  return new Promise(function (resolve, reject) {
+    const next = function () {
+      try {
+        const r = func.apply(self, args)
+        if (!r || !r.then) {
+          return resolve(r)
+        }
+        r.then(resolve).catch(reject)
+      } catch (e) {
+        reject(e)
+      }
+    }
+    tail.then(next).catch(next)
+  })
+}
+
 module.exports = function () {
-  let live = Promise.resolve()
+  let tail = Promise.resolve()
   const wrap = function (func, self) {
     if (typeof func !== 'function') {
       throw new TypeError('"func" argument must be a function')
     }
     return function () {
       const args = Array.prototype.slice.call(arguments)
-      const song = new Promise(function (resolve, reject) {
-        const next = function () {
-          try {
-            const r = func.apply(self, args)
-            if (!r || !r.then) {
-              return resolve(r)
-            }
-            r.then(resolve).catch(reject)
-          } catch (e) {
-            reject(e)
-          }
-        }
-        live.then(next).catch(next)
-      })
-      live = song
+      const song = add(tail, func, self, args)
+      tail = song
       return song
     }
   }
